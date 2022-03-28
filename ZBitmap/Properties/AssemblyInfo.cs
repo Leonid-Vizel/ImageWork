06c1210b3adac955522e8b2b7f4d098636011dad
Vizel Leonid <spartanlaska@gmail.com>
2022-03-28 20:48:53 +0300

    [Renaming] ImageWork->ZBitmap

--- ZBitmap/Properties/AssemblyInfo.cs
+++ ZBitmap/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ImageWork")]
+[assembly: AssemblyTitle("ZBitmap")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageWork")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("ZBitmap")]
+[assembly: AssemblyCopyright("Vizel Leonid Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
